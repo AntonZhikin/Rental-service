b1de95b1c61d133056df0e5f02300e0e15f4b148
AntonZhikin <antonzhikin0@gmail.com>
2025-05-13 21:04:27 +0300

    test with postman(all good)

---
+++ server/controllers/offerController.js
@@ -0,0 +1,3 @@
+export async function getAllOffers(req, res, next) {
+	res.status(200).json({ message: "Заработало!" });
+}
--- server/index.js
+++ server/index.js
@@ -1,15 +1,24 @@
+import cors from "cors";
 import * as dotenv from "dotenv";
 import express from "express";
 import sequelize from "./config/database.js";
 import { Offer } from "./models/offer.js";
 import { Review } from "./models/review.js";
 import { User } from "./models/user.js";
+import { router } from "./routers/index.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/", router);
+
+app.get("/", (req, res) => {
+	res.status(200).json({ message: "Ура!" });
+});
 
 const start = async () => {
 	try {
